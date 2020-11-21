337758cb268bd81a8c0b754c867ffb5473096cfd
LINFANER <42420861+LINFANER@users.noreply.github.com>
2020-11-20 20:40:09 -0500

    Add files via upload

--- app.py
+++ app.py
@@ -2,9 +2,6 @@ import streamlit as st
 import pandas as pd
 import altair as alt
 import numpy as np
-import datetime
-from vega_datasets import data
-import geopandas as gpd
 from wordcloud import WordCloud
 import matplotlib.pyplot as plt
 
@@ -27,11 +24,10 @@ stopwords=nltk.corpus.stopwords.words('english')
 st.title('Fake News Prediction')
 st.header('Understanding Fake News and How Models Distinguish Them')
 
-intro_text="With the current massive use of social media, information quality becomes an important issue when it comes to news reporting and other sources of factuality checking. Given the hot topics in fake news classification and toxic comments classification, many datasets have been collected, and they cover a wide range of domains. For example, the LIAR dataset was collected from Politifact and covers the political topics, while FEVER which covers scientific topics is collected from Wikipedia. As more and more fake news prevails on social media, it is getting harder for people to distinguish between true news and fake news and people could sometimes easily be misled by the seemingly-true fake news. As a result, different fake news prediction models have developed intending to serve as fake news detector and help people perform fact-checking. Many state-of-the-art fake news prediction models have achieved satisfying performance and even outperforming human. This article aims to investigate the characteristics of fake news articles, focusing on a selected fake news dataset, and studying how fake news prediction models distinguish between true news and fake news. This allows people to understand how those models differentiate true news and fake news and would also be beneficial for improving reader's ability in identifying fake news. "
+with open('Introduction.txt', 'r') as f:
+    intro_text=f.readlines()[0]
 
 st.write(intro_text)
-
-
 st.header('On what subjects do people tend to lie?')
 
 
@@ -102,7 +98,6 @@ if absolute=='Percentage':
 combined_table=combined_table.melt(id_vars='kind')
 
 if combine_labels:
-    
     scatter_chart=st.altair_chart(
         alt.Chart(combined_table, width=700).mark_bar().encode(
             x='value:Q',
@@ -112,7 +107,6 @@ if combine_labels:
     )
 
 else:
-
     label_sel=st.selectbox(
         'Select a label value: ',
          label_values)
@@ -128,10 +122,9 @@ else:
         ).interactive()
     )
 
+########################### PART 2 ##############################
 
-
-st.header('Word Frequency and use of punctuation')
-
+st.header('Word Frequency for news with different topics')
 
 def text_lowercase(text): 
     return text.lower() 
@@ -170,13 +163,18 @@ def preprocess_statement(df):
     df['new_statement']=df.new_statement.apply(lambda x: lemmatize_word(x))
     return df
 
-def extract_key_words(df, label, subject):
+def extract_key_words(df, label, meta_feature, value):
     sub_df=df.loc[df.label==label]
-    sub_df=sub_df.loc[sub_df.subject==subject]
+    sub_df=sub_df.loc[sub_df[meta_feature]==value].reset_index(drop=True)
     total_text=sub_df.new_statement.sum()
     return total_text
 
-top_ten_subjects=df_train.subject.value_counts()[:10].index
+
+feature_sel=st.selectbox(
+    'Select a Meta Feature: ',
+     meta_feature, key='meta_feature2')
+
+top_ten_subjects=df_train[feature_sel].value_counts()[:20].index
 
 df_train=preprocess_statement(df_train)
 
@@ -185,23 +183,28 @@ label_sel=st.selectbox(
          label_values, key='label_sel2')
 
 subject_type=st.selectbox(
-        'Select a subject topic: ',
+        'Select a value for the meta feature: ',
          top_ten_subjects)
 
-total_text=extract_key_words(df_train, label_sel, subject_type)
+total_text=extract_key_words(df_train, label_sel, feature_sel, subject_type)
 
-fig=plt.figure(figsize=(10, 5))
-wordcloud=WordCloud(stopwords=stopwords, background_color="white").generate(total_text)
-plt.imshow(wordcloud, interpolation='bilinear')
-plt.axis("off")
-plt.show()
-st.pyplot(fig)
+try:
+    fig=plt.figure(figsize=(10, 5))
+    wordcloud=WordCloud(stopwords=stopwords, background_color="white").generate(total_text)
+    plt.imshow(wordcloud, interpolation='bilinear')
+    plt.axis("off")
+    plt.show()
+    st.pyplot(fig)
+except:
+    st.write('No news found in the database, please try other selection criteria')
 
-st.header('Sentiment analysis on true news and fake news')
+
+########################### PART 3 ##############################
+
+st.header('Overall Sentiment analysis')
 
 
 sid=SentimentIntensityAnalyzer()
-
 
 def analyze_sentiment(sentence):
     score=sid.polarity_scores(sentence)
@@ -210,15 +213,16 @@ def analyze_sentiment(sentence):
 #     print()
     return score
 
-def sentiment_for_news(df, label):
-    sub_df=df.loc[df.label==label].reset_index(drop=True)
+def sentiment_for_news(df, label, meta_feature, value):
+    sub_df=df.loc[df.label==label]
+    sub_df=sub_df.loc[sub_df[meta_feature]==value].reset_index(drop=True)
     scores={}
     for i in range(0, len(sub_df)):
         score=analyze_sentiment(sub_df.statement[i])
         scores['compound']=scores.get('compound',[])+[score['compound']]
-        scores['neg']=scores.get('neg',[])+[score['neg']]
-        scores['neu']=scores.get('neu',[])+[score['neu']]
-        scores['pos']=scores.get('pos',[])+[score['pos']]
+        scores['negative']=scores.get('negative',[])+[score['neg']]
+        scores['neutral']=scores.get('neutral',[])+[score['neu']]
+        scores['positive']=scores.get('positive',[])+[score['pos']]
     return pd.DataFrame.from_dict(scores)
 
 def sentiment_for_type(df, sentiment_type):
@@ -232,23 +236,158 @@ def sentiment_for_type(df, sentiment_type):
     return scores
 
 sentiment_type=['compound','neg','neu','pos']
-sentiment_type_table=sentiment_for_news(df_train, 'true')
-sentiment_type_table
+sentiment_type_table=sentiment_for_news(df_train, label_sel, feature_sel, subject_type)
+# sentiment_type_table
 
-alt.Chart(sentiment_type_table).transform_fold(
-    ['compound',
-     'neg',
-     'neu',
-     'pos'],
-    as_ = ['Sentiments', 'value']
-).transform_density(
-    density='value',
-    bandwidth=0.3,
-    groupby=['Sentiments'],
-    extent= [-2, 5]
-).mark_area().encode(
-    alt.X('value:Q'),
-    alt.Y('density:Q', axis=None),
-    alt.Row('Sentiments:N')
+
+scatter_chart=st.altair_chart(
+    alt.Chart(sentiment_type_table).transform_fold(
+        ['compound',
+         'negative',
+         'neutral',
+         'positive'],
+        as_ = ['Sentiments', 'value']
+    ).transform_density(
+        density='value',
+        bandwidth=0.3,
+        groupby=['Sentiments'],
+        extent= [-2, 3]
+    ).mark_area().encode(
+        alt.X('value:Q'),
+        alt.Y('density:Q', axis=None),
+        alt.Row('Sentiments:N')
+        
+    ).properties(width=400, height=50).interactive()
+)
+
+
+
+
+########################### PART 4 ##############################
+st.header('Other text statistics for news')
+
+import readability
+def get_other_statistics(df, label, if_filter, meta_feature, value):
+    readabilty_grades_list=[]
+    sentence_info_list=[]
+    word_usage_list=[]
+    sentence_beginnings_list=[]
+    for l in label:
+        readabilty_grades=[]
+        sentence_info=[]
+        word_usage=[]
+        sentence_beginnings=[]
+        
+        sub_df=df.loc[df.label==l].reset_index(drop=True)
+        if if_filter:
+            sub_df=sub_df.loc[sub_df[meta_feature]==value].reset_index(drop=True)
+            
+        # loop through all labels
+        for i in range(0, len(sub_df)):
+            results=readability.getmeasures(df_train.new_statement[i], lang='en')
+            readabilty_grades.append(results['readability grades'])
+            sentence_info.append(results['sentence info'])
+            word_usage.append(results['word usage'])
+            sentence_beginnings.append(results['sentence beginnings'])
+        readabilty_grades=pd.DataFrame.from_dict(readabilty_grades)
+        readabilty_grades['label']=l
+        
+        sentence_info=pd.DataFrame.from_dict(sentence_info)
+        sentence_info['label']=l
+        
+        word_usage=pd.DataFrame.from_dict(word_usage)
+        word_usage['label']=l
+        
+        sentence_beginnings=pd.DataFrame.from_dict(sentence_beginnings)
+        sentence_beginnings['label']=l
+        
+        readabilty_grades_list.append(readabilty_grades)
+        sentence_info_list.append(sentence_info)
+        word_usage_list.append(word_usage)
+        sentence_beginnings_list.append(sentence_beginnings)
     
-).properties(width=400, height=50)
+    readabilty_grades_list=pd.concat(readabilty_grades_list)
+    sentence_info_list=pd.concat(sentence_info_list)
+    word_usage_list=pd.concat(word_usage_list)
+    sentence_beginnings_list=pd.concat(sentence_beginnings_list)
+    
+    return readabilty_grades_list, sentence_info_list, word_usage_list, sentence_beginnings_list
+
+def select_df(var1, var2, subcategory, statistic_df, if_filter, n):
+    df=statistic_df[subcategory]
+    sub_df=df[[var1, var2, 'label']]
+    if not if_filter:
+        return sub_df.sample(n=n, random_state=1)
+    return sub_df
+
+filter=st.checkbox('Adding filtering criteria for the data, if no criteria, then n randomly selected data point will be displayed')
+
+if not filter:
+    n_point=st.slider('Select the number of points to display', 1, 5000)
+else:
+    n_point=500
+    feature_sel=st.selectbox(
+        'Select a Meta Feature: ',
+         meta_feature, key='meta_feature3')
+
+    top_ten_subjects=df_train[feature_sel].value_counts()[:20].index
+
+    subject_type=st.selectbox(
+            'Select a value for the meta feature: ',
+             top_ten_subjects, key='value3')
+
+readabilty_grades, sentence_info, word_usage, sentence_beginnings=get_other_statistics(df_train, label_values, filter,feature_sel, subject_type)
+
+statistics_type=['sentence_info', 'readabilty_grades','word_usage','sentence_beginnings']
+statistic_columns={'sentence_info':list(sentence_info.columns[:-1]), 'readabilty_grades': list(readabilty_grades.columns[:-1]), 
+                  'word_usage': list(word_usage.columns[:-1]), 'sentence_beginnings': list(sentence_beginnings.columns[:-1])}
+statistic_df={'sentence_info':sentence_info, 'readabilty_grades': readabilty_grades, 
+                  'word_usage': word_usage, 'sentence_beginnings': sentence_beginnings}
+
+stat_type=st.selectbox('Select a text statistics for exploring', statistics_type)
+
+var1=st.selectbox('Select frist metric for exploring', statistic_columns[stat_type])
+var2=st.selectbox('Select second metric for exploring', statistic_columns[stat_type])
+
+current_df=select_df(var1,var2, stat_type, statistic_df, filter, n_point)
+
+brush = alt.selection(type='interval')
+points = alt.Chart(current_df).mark_point().encode(
+    x='FleschReadingEase:Q',
+    y='Kincaid:Q',
+    color=alt.condition(brush, 'label:N', alt.value('lightgray'))
+).add_selection(
+    brush
+)
+bars = alt.Chart(current_df).mark_bar().encode(
+    y='label:N',
+    color='label:N',
+    x='count(label):Q'
+).transform_filter(
+    brush
+)
+
+brush=alt.selection(type='interval')
+points=alt.Chart(current_df).mark_point().encode(
+    x=var1,
+    y=var2,
+    color=alt.condition(brush, 'label:N', alt.value('lightgray'))
+).add_selection(
+    brush
+)
+bars=alt.Chart(current_df).mark_bar().encode(
+    y='label:N',
+    color='label:N',
+    x='count(label):Q'
+).transform_filter(
+    brush
+)
+
+scatter_chart=st.altair_chart(
+    points&bars
+)
+
+########################### PART 5 ##############################
+st.header('How machine learning models distinguish fake news?')
+st.write('in progress.....')
+
